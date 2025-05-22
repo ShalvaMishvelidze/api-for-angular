c6973c6eb650b273e729476d541c4d37d3d1a084
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-22 16:55:20 +0400

    added products route and mock data for temporarily api

---
+++ app/products/route.ts
@@ -0,0 +1,20 @@
+import { headers } from "@/utils/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Data from "@/assets/Amazing Animal Paintings.json";
+
+export async function OPTIONS() {
+  return new NextResponse(null, { status: 204, headers: headers });
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    // Your logic here
+    return NextResponse.json(Data, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+}
