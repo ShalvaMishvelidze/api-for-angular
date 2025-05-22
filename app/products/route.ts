2d581f284bf53af2b0f95082312b23a242a81f5b
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-22 17:02:43 +0400

    removed unnecessary options export

--- app/products/route.ts
+++ app/products/route.ts
@@ -2,14 +2,10 @@ import { headers } from "@/utils/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Data from "@/assets/Amazing Animal Paintings.json";
 
-export async function OPTIONS() {
-  return new NextResponse(null, { status: 204, headers: headers });
-}
-
 export async function GET(req: NextRequest) {
   try {
     // Your logic here
-    return NextResponse.json(Data, { status: 200 });
+    return NextResponse.json(Data, { status: 200, headers: headers });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
