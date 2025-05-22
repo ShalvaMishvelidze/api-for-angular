0ab7a9b2e01f11454a39b117fa74d0329c634669
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-22 15:25:42 +0400

    set up mock api for my angular project

--- app/reservations/route.ts
+++ app/reservations/route.ts
@@ -1,8 +1,32 @@
+import { headers } from "@/utils/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    return NextResponse.json({ message: "Hello World" }, { status: 200 });
+    return NextResponse.json(
+      [
+        {
+          id: "1",
+          guestName: "John Doe",
+          guestEmail: "john@doe.com",
+          roomNumber: 101,
+          checkInDate: "2023-10-01",
+          checkOutDate: "2023-10-05",
+        },
+        {
+          id: "2",
+          guestName: "Jane Smith",
+          guestEmail: "jane@smith.com",
+          roomNumber: 102,
+          checkInDate: "2023-10-02",
+          checkOutDate: "2023-10-06",
+        },
+      ],
+      {
+        status: 200,
+        headers: headers,
+      }
+    );
   } catch (error) {
     console.error(error);
     return NextResponse.json(
---
+++ utils/headers.ts
@@ -0,0 +1,5 @@
+export const headers = {
+  "Access-Control-Allow-Origin": "http://localhost:4200",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
