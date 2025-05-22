2b054cde4149a98d9bbf0a583aa9eee148110f0b
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-22 15:30:27 +0400

    moved get reservation to dynamic route

--- app/reservation/route.ts
+++ app/reservation/route.ts
@@ -1,29 +1,6 @@
 import { headers } from "@/utils/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  try {
-    // Your logic here
-    return NextResponse.json(
-      {
-        id: "1",
-        guestName: "John Doe",
-        guestEmail: "john@doe.com",
-        roomNumber: 101,
-        checkInDate: "2023-10-01",
-        checkOutDate: "2023-10-05",
-      },
-      { status: 200, headers: headers }
-    );
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Something went wrong" },
-      { status: 500 }
-    );
-  }
-}
-
 export async function POST(req: NextRequest) {
   try {
     // Your logic here
