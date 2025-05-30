989acf33eeee869f4d56f955396ae9eeef18a22a
ShalvaMishvelidze <shalva.mishvelidze111@gmail.com>
2025-05-30 12:02:56 +0400

    updated api to include my schema model and renamed the model

--- app/(e-commerce-app)/cart/route.ts
+++ app/(e-commerce-app)/cart/route.ts
@@ -1,36 +1,10 @@
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_: NextRequest) {
   try {
-    return NextResponse.json(
-      [
-        {
-          id: 1,
-          name: "Dish Soap Dispenser",
-          price: 18.99,
-          image_url: "http://dummyimage.com/108x100.png/ff4444/ffffff",
-        },
-        {
-          id: 2,
-          name: "Carrot and Celery Sticks",
-          price: 2.99,
-          image_url: "http://dummyimage.com/101x100.png/cc0000/ffffff",
-        },
-        {
-          id: 3,
-          name: "Organic Coconut Sugar",
-          price: 4.19,
-          image_url: "http://dummyimage.com/245x100.png/5fa2dd/ffffff",
-        },
-        {
-          id: 4,
-          name: "Tomato Basil Pasta Sauce",
-          price: 3.99,
-          image_url: "http://dummyimage.com/235x100.png/ff4444/ffffff",
-        },
-      ],
-      { status: 200 }
-    );
+    const products = await prisma.cart.findMany();
+    return NextResponse.json(products, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
@@ -43,7 +17,10 @@ export async function GET(_: NextRequest) {
 export async function POST(req: NextRequest) {
   const product = await req.json();
   try {
-    return NextResponse.json(product, { status: 200 });
+    const newProduct = await prisma.cart.create({
+      data: product,
+    });
+    return NextResponse.json(newProduct, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.json(
@@ -55,6 +32,7 @@ export async function POST(req: NextRequest) {
 
 export async function DELETE(_: NextRequest) {
   try {
+    await prisma.cart.deleteMany();
     return new NextResponse(null, { status: 204 });
   } catch (error) {
     console.error(error);
--- app/(e-commerce-app)/checkout/route.ts
+++ app/(e-commerce-app)/checkout/route.ts
@@ -1,7 +1,9 @@
+import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(_: NextRequest) {
   try {
+    await prisma.cart.deleteMany();
     // Your logic here
     return NextResponse.json("Checkout Successful!", { status: 200 });
   } catch (error) {
